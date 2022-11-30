0123651e170338b1c12f6609e37728f415248c51
Álvaro Costa <alvaroc.dev@gmail.com>
2022-11-30 20:09:47 -0300

    part02: courseinfo2: add exercise 5

--- part02/courseinfo2/src/App.js
+++ part02/courseinfo2/src/App.js
@@ -1,32 +1,4 @@
-const Header = (props) => (
-  <h2>{props.course}</h2>
-);
-
-const Part = (props) => (
-  <p>
-    {props.part.name} {props.part.exercises}
-  </p>
-);
-
-const Content = ({ parts }) => (
-  <div>
-    { parts.map((part) => <Part key={part.id} part={part} />) }
-  </div>
-);
-
-const Total = ({ parts }) => (
-  <p>
-    <strong>total of {parts.reduce((sum, cur) => sum + cur.exercises, 0)} exercises</strong>
-  </p>
-);
-
-const Course = ({course}) => (
-  <div>
-    <Header course={course.name} />
-    <Content parts={course.parts} />
-    <Total parts={course.parts}/>
-  </div>
-);
+import Course from './Course';
 
 const App = () => {
   const courses = [
