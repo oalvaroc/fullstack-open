fc4d2239958ed77718abbc7705b752a8b9bbee84
Álvaro Costa <alvaroc.dev@gmail.com>
2022-12-21 12:47:45 -0300

    part03: phonebook-db: add exercise 16 (step 3)

--- part03/phonebook-backend/src/index.js
+++ part03/phonebook-backend/src/index.js
@@ -15,29 +15,32 @@ morgan.token('body', (req, res) => {
 });
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-app.get('/api/persons', (req, res) => {
+app.get('/api/persons', (req, res, next) => {
     PersonModel
         .find({})
         .then((persons) => {
             res.json(persons);
-        });
+        })
+        .catch((err) => next(err));
 });
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
     PersonModel
         .findById(req.params.id)
-        .then((person) => res.json(person));
+        .then((person) => res.json(person))
+        .catch((err) => next(err));
 });
 
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
     PersonModel
         .findByIdAndDelete(req.params.id)
         .then(() => {
             res.status(204).end();
-        });
+        })
+        .catch((err) => next(err));
 });
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
     const body = req.body;
     if (!body.name) {
         res.status(400).json({ error: 'missing name' });
@@ -53,18 +56,33 @@ app.post('/api/persons', (req, res) => {
             name: body.name,
             number: body.number
         });
-        person.save().then((p) => res.json(p));
+        person.save()
+            .then((p) => res.json(p))
+            .catch((err) => next(err));
     }
 });
 
-app.get('/info', (req, res) => {
+app.get('/info', (req, res, next) => {
     PersonModel
         .find({})
         .then((persons) => {
             res.send(`<p>Phonebook has info for ${persons.length} ${persons.length > 1 ? 'people' : 'person'}</p>
                       <p>${new Date()}</p>`);
-        });
+        })
+        .catch((err) => next(err));
 });
+
+function errorHandler(error, req, res, next) {
+    console.error(error);
+
+    if (error.name === 'CastError') {
+        return res.status(400).json({ error: 'malformatted id' });
+    }
+
+    next(error);
+}
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
